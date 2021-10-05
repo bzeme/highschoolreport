67ef56f8e6b7a1551b9ef2e6b8652476ae03816f
bzeme <ubzeme@gmail.com>
2021-10-05 12:02:20 +0800

    Delete 程式碼 directory

--- 程式碼/input.h
+++
@@ -1,7 +0,0 @@
-/* 各函數功能請參閱 input.c */
-unsigned char get_keycode();
-unsigned char keycode_to_num(char code);
-char wait_for_code();
-unsigned char num_to_keycode(char code);
-unsigned int enter_number(char n, __bit first, unsigned char org,
-			  __bit allow_enter, __bit auto_submit);
